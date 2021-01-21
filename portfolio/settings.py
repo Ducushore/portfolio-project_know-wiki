862e9bf80d58314e9171da555687c40521054ba5
Ducushore <listedbelow67@gmail.com>
2021-01-21 12:10:10 +0000

    getting the site to run on the internet with Gunicorn and Nginx

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -159,6 +159,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')  # may work with
 MEDIA_URL = '/media/'
 
 try:
-    from local_settings import *
+    from .local_settings import *
 except ImportError:
     pass
